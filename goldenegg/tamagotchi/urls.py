2c94d9b9514a3d82ebf1e33d9811180e1fc85cfe
kuro-kuris <cswart@outlook.com>
2015-10-11 15:52:44 +0100

    presentation stuff

--- goldenegg/tamagotchi/urls.py
+++ goldenegg/tamagotchi/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     # ex: /tamagotchi/
     url(r'^$', views.index, name='index'),
+    # ex: /tamagotchi/homepage/
+    url(r'^/homepage/$', views.homepage, name='homepage'),
+    # ex: /tamagotchi/choose/
+    url(r'^/choose/$', views.choose, name='choose'),
     # ex: /tamagotchi/5/
     url(r'^(?P<user_id>[0-9]+)/$', views.user, name='user'),
     # ex: /tamagotchi/5/pet/
--- goldenegg/tamagotchi/views.py
+++ goldenegg/tamagotchi/views.py
@@ -4,17 +4,33 @@ from django.shortcuts import get_object_or_404,render
 from .models import User, Pet
 
 
+# def index(request):
+#     richest_pet_list = Pet.objects.order_by('virtual_gold')[:5]
+#     highest_level_pet_list = Pet.objects.order_by('level','experience')[:5]
+#     context = {
+#         'richest_pet_list': richest_pet_list,
+#         'highest_level_pet_list': highest_level_pet_list,
+#     }
+#     return render(request, 'tamagotchi/index.html', context)
+
 def index(request):
-    richest_pet_list = Pet.objects.order_by('virtual_gold')[:5]
-    highest_level_pet_list = Pet.objects.order_by('level','experience')[:5]
+    user = Pet.objects.order_by('virtual_gold')[0]
     context = {
-        'richest_pet_list': richest_pet_list,
-        'highest_level_pet_list': highest_level_pet_list,
+        'user': user,
     }
     return render(request, 'tamagotchi/index.html', context)
 
 
+def choose(request):
+    context = {}
+    return render(request, 'tamagotchi/choose-character.html', context)
 
+def homepage(request):
+    user = Pet.objects.order_by('virtual_gold')[0].user
+    context = {
+        'user': user,
+    }
+    return render(request, 'tamagotchi/homepage.html', context)
 
 def user(request, user_id):
     user = get_object_or_404(User, pk=user_id)
